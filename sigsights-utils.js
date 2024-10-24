e2c3a72d68111b8e12ccbd07863c9fa764088f92
Ryan Miller <ryan.l.miller@icloud.com>
2024-10-24 14:56:30 -0700

    a little clean up

--- sigsights-utils.js
+++ sigsights-utils.js
@@ -127,7 +127,6 @@ function showLoginAndRegistrationBySite(d) {
     }
 
     Plotly.newPlot('loginAndRegistrationBySite', data, defaultStackBarChartLayout(), defaultChartOptions());
-
 }
 
 function showAnomalySignalsBySite(d) {
